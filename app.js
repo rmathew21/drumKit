add6ca1f35f0c503f88bc35cf942187de6e3d588
rob <robmathew21@gmail.com>
2024-12-03 21:39:41 -0600

    fixed index to have data-key match between div and audio

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    console.log(audio);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
+    if(!audio) return; // stop the function from running all together
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    console.log(key);
 })
